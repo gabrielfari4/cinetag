35cdb3770af4a8eb8f4a3030f44eea929c34f612
gabrielfari4 <gabrielfaria88@gmail.com>
2025-05-08 17:03:51 -0300

    :lipstick: feat: estilizando Favoritos

--- src/pages/Favoritos/index.jsx
+++ src/pages/Favoritos/index.jsx
@@ -1,8 +1,18 @@
+import Banner from '../../components/Banner';
+import Card from '../../components/Card';
+import Titulo from '../../components/Titulo';
 import styles from './Favoritos.module.css';
 
 const Favoritos = () => {
     return (
         <>
+            <Banner imagem='favoritos' />
+            <Titulo>
+                <h1>Meus favoritos</h1>
+            </Titulo>
+            <section className={styles.container}>
+                <Card id='1' titulo='Qualquer' capa='' />
+            </section>
         </>
     )
 }
