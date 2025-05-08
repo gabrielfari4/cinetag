7f01f1d3d73050f706820938545a8f04b4467d67
gabrielfari4 <gabrielfaria88@gmail.com>
2025-05-08 16:31:58 -0300

    :sparkles: feat: criando Titulo

--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -2,12 +2,14 @@ import Cabecalho from '@/components/Cabecalho';
 import styles from './Inicio.module.css';
 import Rodape from '../../components/Rodape';
 import Banner from '../../components/Banner';
+import Titulo from '../../components/Titulo';
 
 const Inicio = () => {
     return (
         <>
         <Cabecalho />
         <Banner imagem='home' />
+        <Titulo><h1>Um lugar para guardar seus vídeos e filmes</h1></Titulo>
         <h1>Nova página</h1>
         <Rodape />
         </>
