1055f3b1d2a9f2a324a9af3101941c429e8fd4a1
gabrielfari4 <gabrielfaria88@gmail.com>
2025-05-08 16:59:38 -0300

    :sparkles: feat: criando Container

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,19 +2,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./pages/Inicio";
 import Favoritos from "./pages/Favoritos";
 import Rodape from "./components/Rodape";
-import Cabecalho from './components/Cabecalho';
+import Cabecalho from "./components/Cabecalho";
+import Container from "./components/Container";
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
-        <Cabecalho />
-            <Routes>
-                <Route path="/" element={<Inicio />} />
-                <Route path="/favoritos" element={<Favoritos />} />
-            </Routes>
+            <Cabecalho />
+            <Container>
+                <Routes>
+                    <Route path="/" element={<Inicio />} />
+                    <Route path="/favoritos" element={<Favoritos />} />
+                </Routes>
+            </Container>
             <Rodape />
         </BrowserRouter>
-    )
-}
+    );
+};
 
 export default AppRoutes;
