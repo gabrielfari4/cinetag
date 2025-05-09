c2c5f9bf6862e79980ee0dea786d52277b583c26
gabrielfari4 <gabrielfaria88@gmail.com>
2025-05-08 21:46:15 -0300

    :sparkles: feat: função favoritar

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,13 +1,24 @@
 import styles from './Card.module.css';
 import iconeFavoritar from './favoritar.png';
 import iconeDesfavoritar from './desfavoritar.png';
+import { useFavoritosContext } from '../../hooks/useFavoritosContext';
 
 const Card = ({ id, titulo, capa }) => {
+    const {favorito, adicionarFavorito} = useFavoritosContext();
+    const isFavorite = favorito.some((fav) => fav.id === id)
+    const icone = isFavorite ? iconeDesfavoritar : iconeFavoritar
     return (
         <div className={styles.container}>
             <img src={capa} alt={titulo} className={styles.capa} />
             <h2>{titulo}</h2>
-            <img src={iconeFavoritar} alt="Favoritar" className={styles.favoritar}/>
+            <img 
+                src={icone} 
+                alt="Favoritar" 
+                className={styles.favoritar}
+                onClick={() => {
+                    adicionarFavorito({id, titulo, capa})
+                }}
+            />
         </div>
     )
 }
